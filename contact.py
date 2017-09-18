b0f2b6d9468749955bd464f0856ad23199cd38a9
ardenn <mc_alila@outlook.com>
2017-09-18 17:53:10 +0000

    Added ContactManager object and its addContact, searchContact, deleteContact, displayContacts methods Added a loop to accept user input and use the input to create Contact instances, and ask the user for further direction

--- contact.py
+++ contact.py
@@ -1,3 +1,6 @@
+import sys
+
+
 class Contact:
     """docstring for Contact"""
 
@@ -16,11 +19,78 @@ class Contact:
     	Work Phone: {}
     	""".format(self.name, self.email, self.cell_phone, self.work_phone)
 
-print("New Contact")
-c_name = input("Enter name: ")
-c_email = input("Enter email: ")
-cell = input("Enter cell phone number: ")
-work = input("Enter work phone number: ")
 
-new_contact = Contact(c_name, c_email, cell, work)
-print(new_contact)
+class ContactManager:
+
+    def __init__(self, contacts=[]):
+        self.contacts = contacts
+
+    def addContact(self, contact):
+        self.contacts.append(contact)
+        my_manager.displayContacts()
+
+    def deleteContact(self):
+        del_email = input("Enter Contact Email: ")
+        for contact in self.contacts:
+            if contact.email == del_email:
+                self.contacts.remove(contact)
+                return contact
+        else:
+            return "Sorry that contact does not exist!!"
+
+    def searchContact(self):
+        search_email = input("Enter Contact Email: ")
+        for contact in self.contacts:
+            if contact.email == search_email:
+                return contact
+        else:
+            return "Sorry that contact does not exist!!"
+
+    def displayContacts(self):
+        for contact in self.contacts:
+            print(contact)
+
+
+if __name__ == "__main__":
+    my_manager = ContactManager()
+
+
+while True:
+    print("""Welcome, How may I help?
+				1: Add Contact
+				2: Search Contact
+				3: Delete Contact
+				4: Exit
+		""")
+    response = int(input("Enter Response: "))
+    if response in [1, 2, 3, 4]:
+        if response == 1:
+
+                # User input for new Contact
+            print("New Contact")
+            c_name = input("Enter name: ")
+            c_email = input("Enter email: ")
+            cell = input("Enter cell phone number: ")
+            work = input("Enter work phone number: ")
+
+            my_manager.addContact(Contact(c_name, c_email, cell, work))
+        elif response == 2:
+            print(my_manager.searchContact())
+        elif response == 3:
+            my_manager.deleteContact()
+            my_manager.displayContacts()
+        else:
+            print("Exiting...")
+            break
+        print("""Do you wish to do something else?
+         				1. Yes
+         				2. No
+         		""")
+        response = int(input("Enter Response: "))
+        if response == 1:
+            continue
+        else:
+            break
+    else:
+        print("Please Enter a valid choice!!")
+        continue
