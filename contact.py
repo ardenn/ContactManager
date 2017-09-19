b36d89ff7bc831b13949e6b74eb28fa3f624a73f
ardenn <mc_alila@outlook.com>
2017-09-19 18:15:27 +0000

    Expanded ContactManager to write and read contact data from a file

--- contact.py
+++ contact.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Contact:
     """docstring for Contact"""
 
@@ -26,8 +29,7 @@ class ContactManager:
         self.contacts.append(contact)
         my_manager.displayContacts()
 
-    def deleteContact(self):
-        del_email = input("Enter Contact Email: ")
+    def deleteContact(self, del_email):
         for contact in self.contacts:
             if contact.email == del_email:
                 self.contacts.remove(contact)
@@ -36,19 +38,37 @@ class ContactManager:
         else:
             return "Sorry that contact does not exist!!"
 
-    def searchContact(self):
-        search_name = input("Enter Contact Name: ")
+    def searchContact(self, search_name):
         print("Your search matched the following:")
         for contact in self.contacts:
             if search_name in contact.name:
                 print(contact)
         else:
-            return "Sorry that contact does not exist!!"
+            print("Sorry that contact does not exist!!")
 
     def displayContacts(self):
         for contact in self.contacts:
             print(contact)
 
+    def readFromFile(self, filename):
+        with open(filename) as read_file:
+            header = read_file.readline()
+
+            for line in read_file.readlines():
+                values = line.strip().split(",")
+                self.contacts.append(Contact(*values))
+        return self.displayContacts()
+
+    def writeToFile(self, filename):
+        with open(filename, "w") as write_file:
+            write_file.write("name,email,cell_phone,work_phone\n")
+
+            for contact in self.contacts:
+                contact_dict = contact.__dict__
+                write_file.write(
+                    "{name},{email},{cell_phone},{work_phone}\n".format(
+                        **contact_dict))
+        self.displayContacts()
 
 if __name__ == "__main__":
     my_manager = ContactManager()
@@ -56,27 +76,35 @@ if __name__ == "__main__":
 
 while True:
     print("""Welcome, How may I help?
-				1: Add Contact
-				2: Search Contact
-				3: Delete Contact
-				4: Exit
-		""")
+    0: Read Contacts from file
+	1: Add Contact
+	2: Search Contact
+	3: Delete Contact
+	4: Write Contacts to file
+	5: Exit
+	""")
     response = int(input("Enter Response: "))
-    if response in [1, 2, 3, 4]:
-        if response == 1:
-
-                # User input for new Contact
+    if response in [0, 1, 2, 3, 4]:
+        if response == 0:
+            filename = input("Enter Filename to read from: ")
+            my_manager.readFromFile(filename)
+        elif response == 1:
+            # User input for new Contact
             print("New Contact")
             c_name = input("Enter name: ")
             c_email = input("Enter email: ")
             cell = input("Enter cell phone number: ")
             work = input("Enter work phone number: ")
-
             my_manager.addContact(Contact(c_name, c_email, cell, work))
         elif response == 2:
-            my_manager.searchContact()
+            search_name = input("Enter Contact Name: ")
+            my_manager.searchContact(search_name)
         elif response == 3:
-            print(my_manager.deleteContact())
+            del_email = input("Enter Contact Email: ")
+            print(my_manager.deleteContact(del_email))
+        elif response == 4:
+            filename = input("Enter Filename to write to: ")
+            my_manager.writeToFile(filename)
         else:
             print("Exiting...")
             break
